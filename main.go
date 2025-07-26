8de6cdcebfc5b921727e6069c2f0ef4c9f762234
ojipoetra <ojishefals2@gmail.com>
2025-07-26 21:07:04 +0700

    route hendling

--- main.go
+++ main.go
@@ -1,15 +1,105 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// router.GET("/ping", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	router.GET("/", homePage)
+	router.GET("/tasks", getTasks)
+	router.GET("/tasks/:id", getTask)
+	router.DELETE("/tasks/:id", removeTask)
+	router.PUT("/tasks/:id", updateTask)
+	router.POST("/tasks", addTask)
 	router.Run()
+
+	// func homePage(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK ,200, gin.H{"message": "Welcome to the Task Manager API"})
+	// }
+
+}
+
+func homePage(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "Welcome to the Task Manager API"})
+}
+
+func getTasks(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
+}
+
+func getTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+	for _, task := range tasks {
+		if task.ID == id {
+			ctx.JSON(http.StatusOK, task)
+			return
+		}
+	}
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+func removeTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			ctx.JSON(http.StatusOK, gin.H{"message": "Task deleted successfully"})
+			return
+		}
+	}
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+func updateTask(ctx *gin.Context) {
+	id := ctx.Param("id")
+	var updateTask Task
+	if err := ctx.ShouldBindJSON(&updateTask); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	for i, task := range tasks {
+		if task.ID == id {
+			if updateTask.Title != "" {
+				tasks[i].Title = updateTask.Title
+			}
+			if updateTask.Description != "" {
+				tasks[i].Description = updateTask.Description
+			}
+			ctx.JSON(http.StatusOK, gin.H{"message": "Task updated"})
+			return
+		}
+	}
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "Task not found"})
+}
+
+func addTask(ctx *gin.Context) {
+	var newTask Task
+	if err := ctx.ShouldBindJSON(&newTask); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	tasks = append(tasks, newTask)
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Task added successfully", "task": newTask})
+}
+
+type Task struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	DueDate     time.Time `json:"due_date"`
+	Status      string    `json:"status"`
+}
+
+var tasks = []Task{
+	{ID: "1", Title: "Task Manager Project", Description: "Add/View/Delete Tasks", DueDate: time.Now(), Status: "In Progress"},
+	{ID: "2", Title: "Books Management Project", Description: "Add/View/Delete Books", DueDate: time.Now().AddDate(0, 0, -1), Status: "Completed"},
 }
